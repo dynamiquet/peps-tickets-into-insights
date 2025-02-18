3f1948801bec3c30b7b34be2e07a52440ea2b34c
Dynamique Twizere <twizeredynamique@gmail.com>
2025-02-17 18:45:35 -0600

    Stop tracking draft.js

--- draft.js
+++
@@ -1,9 +0,0 @@
-var hi = SpreadSheetApp.getActiveSpreadS
-
-
-
-// Tue Nov 01 2022 04:00:00 GMT-0500 (Central Daylight Time), Tue Nov 01 2022 04:00:00 GMT-0500 (Central Daylight Time), Tue Nov 01 2022 04:00:00 GMT-0500 (Central Daylight Time), Tue Nov 01 2022 04:00:00 GMT-0500 (Central Daylight Time), Tue Nov 01 2022 04:00:00 GMT-0500 (Central Daylight Time), Tue Nov 01 2022 04:00:00 GMT-0500 (Central Daylight Time), Tue Nov 01 2022 04:00:00 GMT-0500 (Central Daylight Time)
-
-// Sat Oct 15 2022 09:00:00 GMT-0500 (Central Daylight Time), Sat Oct 15 2022 09:00:00 GMT-0500 (Central Daylight Time)
-
-//Fri Sep 30 2022 13:30:00 GMT-0500 (Central Daylight Time), Fri Sep 30 2022 13:30:00 GMT-0500 (Central Daylight Time)
